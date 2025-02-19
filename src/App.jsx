4b8838e57b814574f89e61babc1617095a0ac5b2
Kamoliddin <kamoliddin3425@gmail.com>
2025-02-19 16:44:11 +0500

    css update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <header>
         <nav>
           <div className="container">
-            <div className="logo"><img src="./public/logo.png" alt="" /></div>
+            <div className="logo"><img src="/logo.png" alt="" /></div>
             <ul>
               <li><a href="">Home</a></li>
               <li><a href="">About</a></li>
@@ -20,11 +20,11 @@ function App() {
               <li><a href="">Contact</a></li>
             </ul>
             <div className="icons">
-              <img src="./public/soroq.svg" alt="" />
-              <img src="./public/eathr.svg" alt="" />
-              <img src="./public/user.svg" alt="" />
+              <img src="/soroq.svg" alt="" />
+              <img src="/eathr.svg" alt="" />
+              <img src="/user.svg" alt="" />
             </div>
-            <p>Menu <img src="../public/menu.svg" alt="" /></p>
+            <p>Menu <img src="/menu.svg" alt="" /></p>
           </div>
         </nav>
         <div className="hero">
@@ -43,14 +43,14 @@ function App() {
       </header>
       <main>
         <section className="our-mission">
-          <img className='section-bg2' src="./public/bg2.png" alt="" />
+          <img className='section-bg2' src="/bg2.png" alt="" />
           <div className="container">
             <div className="section-img">
               <img className='car-img' src="/img1.png" alt="" />
               <img className='img1' src="/img2.png" alt="" />
             </div>
             <div className="section-info">
-              <img src="./public/Vector.png" alt="" />
+              <img src="/Vector.png" alt="" />
               <h1 className='section-title'>Our Mission</h1>
               <p>At Astro, we are driven by the vision of a sustainable future where transportation is both environmentally friendly and exhilarating</p>
               <p>Our mission is to revolutionize the mobility landscape by developing innovative electric vehicles (EVs) that combine cutting-edge technology with dynamic performance. </p>
@@ -71,14 +71,14 @@ function App() {
                 <h1>Astro 1</h1>
                 <div className="price">
                   <p>Starts at <span>$39,000</span></p>
-                  <p>Learn more <img src="./public/right.svg" alt="" /></p>
+                  <p>Learn more <img src="/right.svg" alt="" /></p>
                 </div>
               </div>
               <div className="card card2">
                 <h1>Astro 2</h1>
                 <div className="price">
                   <p>Starts at <span>$59,000</span></p>
-                  <p>Learn more <img src="./public/right.svg" alt="" /></p>
+                  <p>Learn more <img src="/right.svg" alt="" /></p>
                 </div>
               </div>
             </div>
@@ -90,28 +90,28 @@ function App() {
               <h1 className='find-title'>Find a dealer</h1>
               <p>To find the best offers and nearby inventory, we need to find your location. Please enter your location</p>
               <div className="search">
-                <img src="./public/search.svg" alt="" />
+                <img src="/search.svg" alt="" />
                 <input type="text" placeholder='Search your location' />
-                <img src="./public/clos.svg" alt="" />
+                <img src="/clos.svg" alt="" />
               </div>
               <div className="or">
-                <img src="./public/line.png" alt="" />
+                <img src="/line.png" alt="" />
                 <p>or</p>
-                <img src="./public/line.png" alt="" />
+                <img src="/line.png" alt="" />
               </div>
-              <button><img src="./public/loc.svg" alt="" />Use my current location</button>
+              <button><img src="/loc.svg" alt="" />Use my current location</button>
             </div>
           </div>
         </section>
         <section className="full-section">
           <div className="container">
             <div className="section-info">
-              <h1>Come and build a sustainable future with us. Join <img src="./public/Vector.png" alt="" /></h1>
+              <h1>Come and build a sustainable future with us. Join <img src="/Vector.png" alt="" /></h1>
               <p>Our company is seeking pasionate, talented, and driven people who care  about what they do. </p>
               <button>Explore open positions</button>
             </div>
             <div className="section-img">
-              <img src="./public/full-img.png" alt="" />
+              <img src="/full-img.png" alt="" />
             </div>
           </div>
         </section>
@@ -120,13 +120,13 @@ function App() {
         <div className="container">
           <p className='ptag'>© 2024 Astra. All right reserved</p>
           <ul>
-            <li><a href=""><img src="./public/sms.svg" alt="" />hello@astra.com</a></li>
+            <li><a href=""><img src="/sms.svg" alt="" />hello@astra.com</a></li>
             <li><a href="">Facebook</a></li>
             <li><a href="">Instagram</a></li>
             <li><a href="">Twitter</a></li>
           </ul>
-          <img className='img-none' src="./public/lin2.png" alt="" />
-          <img className='linefull' src="../public/Line 15.png" alt="" />
+          <img className='img-none' src="/lin2.png" alt="" />
+          <img className='linefull' src="/Line 15.png" alt="" />
           <p>Privacy Policy</p>
           <p className='ptag2'>© 2024 Astra. All right reserved</p>
         </div>
